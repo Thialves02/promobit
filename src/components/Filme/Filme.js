c4257b1b3a27622d228a1f61aecac998de015890
Thiago <thiago.ralves02@gmail.com>
2022-02-01 23:44:13 -0300

    fix: date formatted

--- src/components/Filme/Filme.js
+++ src/components/Filme/Filme.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./Filme.css";
+import { parseISO, format, formatRelative, formatDistance } from "date-fns";
+
+import ptBR from "date-fns/locale/pt-BR";
 
 export default function Filme({ imagem, titulo, data }) {
+  const firstDate = parseISO(data);
+  const formattedDate = format(firstDate, "' ' dd ' ' MMM' ' yyyy' '", {
+    locale: ptBR,
+  });
+
   return (
     <div className="filme-container">
       <img src={`https://image.tmdb.org/t/p/w300${imagem}`} />
       <h2>{titulo}</h2>
-      <h3>{data}</h3>
+      <h3>{formattedDate}</h3>
     </div>
   );
 }
