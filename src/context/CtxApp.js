f98d95043dbadc23898e3d10c7c9992a30cc3d8f
Thiago <thiago.ralves02@gmail.com>
2022-02-02 21:05:24 -0300

    feat: new item added to context

--- src/context/CtxApp.js
+++ src/context/CtxApp.js
@@ -5,9 +5,9 @@ const Context = createContext();
 
 export default function CtxApp({ children }) {
   const [pagina, setPagina] = useState(1);
-
+  const [filmes, setFilmes] = useState([]);
   return (
-    <Context.Provider value={{ pagina, setPagina }}>
+    <Context.Provider value={{ pagina, setPagina, filmes, setFilmes }}>
       {children}
     </Context.Provider>
   );
