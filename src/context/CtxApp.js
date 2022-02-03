eadd446411a1a3c24d18281d6f91d8e117d20d14
Thiago <thiago.ralves02@gmail.com>
2022-02-03 13:02:16 -0300

    feat: fillmes filtrados added to context

--- src/context/CtxApp.js
+++ src/context/CtxApp.js
@@ -6,8 +6,19 @@ const Context = createContext();
 export default function CtxApp({ children }) {
   const [pagina, setPagina] = useState(1);
   const [filmes, setFilmes] = useState([]);
+  const [filmesFiltrados, setFilmesFiltrados] = useState([]);
+
   return (
-    <Context.Provider value={{ pagina, setPagina, filmes, setFilmes }}>
+    <Context.Provider
+      value={{
+        pagina,
+        setPagina,
+        filmes,
+        setFilmes,
+        filmesFiltrados,
+        setFilmesFiltrados,
+      }}
+    >
       {children}
     </Context.Provider>
   );
