bf7cc414aac9c03a62ab2edf2f28bf1c7305141c
Thiago <thiago.ralves02@gmail.com>
2022-02-03 13:00:57 -0300

    feat: button change  when clicked

--- src/components/Categoria/Categoria.js
+++ src/components/Categoria/Categoria.js
@@ -1,11 +1,13 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../../context/CtxApp";
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Categoria.css";
 
 export default function Categoria({ categoria, index, id }) {
-  const { filmes, setFilmes } = useContext(Context);
-  const [filmesFiltrados, setFilmesFiltrados] = useState([]);
-
+  const { filmes, setFilmes, filmesFiltrados, setFilmesFiltrados } =
+    useContext(Context);
+  const [active, setActive] = useState(false);
   let filtro = JSON.parse(localStorage.getItem("filtro")) || [];
 
   const filtrarGeneros = () => {
@@ -13,8 +15,10 @@ export default function Categoria({ categoria, index, id }) {
       const index = filtro.indexOf(id);
       if (index != -1) {
         filtro.splice(index, 1);
+        setActive(!active);
       } else {
         filtro.push(id);
+        setActive(!active);
       }
       localStorage.setItem("filtro", JSON.stringify(filtro));
 
@@ -23,13 +27,11 @@ export default function Categoria({ categoria, index, id }) {
     filtrar();
     const con = JSON.parse(localStorage.getItem("filtro"));
     const carregarFilmes = async () => {
-      console.log(con);
       const response = await fetch(
         `https://api.themoviedb.org/3/discover/movie?api_key=83a924a233a6fae4e8bb3ece72e1dcd0&with_genres=${con}`
       );
       const body = await response.json();
       setFilmes(body.results);
-      console.log(body.results);
     };
     carregarFilmes();
     //USAR LOCALSTORAGE
@@ -37,8 +39,19 @@ export default function Categoria({ categoria, index, id }) {
   };
 
   return (
-    <div key={index} onClick={filtrarGeneros}>
-      <button>{categoria}</button>
+    <div
+      key={index}
+      onClick={filtrarGeneros}
+      className={
+        active == false ? "categoria-button" : "categoria-button__active"
+      }
+    >
+      <button>
+        {categoria}
+        <div className="categoria-button__icon">
+          <FontAwesomeIcon icon={faTimesCircle} />
+        </div>
+      </button>
     </div>
   );
 }
