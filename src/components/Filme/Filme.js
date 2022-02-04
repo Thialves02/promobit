de0454a63d9b92d74ca1a332e3d32b103f7b1411
Thiago <thiago.ralves02@gmail.com>
2022-02-03 21:59:27 -0300

    feat: link to route filme added

--- src/components/Filme/Filme.js
+++ src/components/Filme/Filme.js
@@ -3,18 +3,21 @@ import "./Filme.css";
 import { parseISO, format } from "date-fns";
 
 import ptBR from "date-fns/locale/pt-BR";
+import { Link } from "react-router-dom";
 
-export default function Filme({ imagem, titulo, data }) {
+export default function Filme({ imagem, titulo, data, id }) {
   const firstDate = parseISO(data);
   const formattedDate = format(firstDate, "' ' dd ' ' MMM' ' yyyy' '", {
     locale: ptBR,
   });
 
   return (
-    <div className="filme-container">
-      <img src={`https://image.tmdb.org/t/p/w300${imagem}`} />
-      <h2>{titulo}</h2>
-      <h3>{formattedDate}</h3>
-    </div>
+    <Link to={`/filme/${id}`}>
+      <div className="filme-container">
+        <img src={`https://image.tmdb.org/t/p/w300${imagem}`} />
+        <h2>{titulo}</h2>
+        <h3>{formattedDate}</h3>
+      </div>
+    </Link>
   );
 }
