67362b3633354a4d111aaffe00b01b3b56c91415
Thiago <thiago.ralves02@gmail.com>
2022-02-03 22:00:37 -0300

    feat: link route added to home

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "../../assets/images/logo.svg";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <header className="navbar-container">
-      <img src={logo} />
+      <Link to="/">
+        <img src={logo} />
+      </Link>
     </header>
   );
 }
