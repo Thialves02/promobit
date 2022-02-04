af5be7a1ad42cc20a7a0ff3abc3a52e31f1036d0
Thiago <thiago.ralves02@gmail.com>
2022-02-03 21:59:07 -0300

    feat: new Router filme added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./pages/Home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import CtxApp from "./context/CtxApp";
+import VerFilme from "./pages/VerFilme/VerFilme";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Navbar />
           <Switch>
             <Route path="/" exact={true} component={Home} />
+            <Route path="/filme/:id" exact={true} component={VerFilme} />
           </Switch>
         </Router>
       </CtxApp>
