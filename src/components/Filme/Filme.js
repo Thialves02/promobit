f918b3284e9543f41ec14f431fa5934fe456204b
Thiago <thiago.ralves02@gmail.com>
2022-02-05 16:04:21 -0300

    fix: recomendations not refreshing fixed

--- src/components/Filme/Filme.js
+++ src/components/Filme/Filme.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Filme.css";
 import { parseISO, format } from "date-fns";
 
 import ptBR from "date-fns/locale/pt-BR";
 import { Link } from "react-router-dom";
+import { Context } from "../../context/CtxApp";
 
 export default function Filme({ imagem, titulo, data, id }) {
+  const { refresh, setRefresh } = useContext(Context);
   const firstDate = parseISO(data);
   const formattedDate = format(firstDate, "' ' dd ' ' MMM' ' yyyy' '", {
     locale: ptBR,
   });
+  const Refresh = () => {
+    setRefresh(!refresh);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
-    <Link to={`/filme/${id}`}>
+    <Link to={`/filme/${id}`} onClick={() => Refresh()}>
       <div className="filme-container">
         <img src={`https://image.tmdb.org/t/p/w300${imagem}`} />
         <h2>{titulo}</h2>
