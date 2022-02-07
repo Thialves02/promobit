db3eb1f23c0f786a284e631f74494fd4ecd3d63f
Thiago <thiago.ralves02@gmail.com>
2022-02-07 12:37:29 -0300

    fixed: multiples requests fixed

--- src/components/Categoria/Categoria.js
+++ src/components/Categoria/Categoria.js
@@ -5,8 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Categoria.css";
 
 export default function Categoria({ categoria, index, id }) {
-  const { setFilmes, filmesFiltrados, setFilmesFiltrados, pagina, API_KEY } =
-    useContext(Context);
+  const { filmesFiltrados, setFilmesFiltrados } = useContext(Context);
   const [active, setActive] = useState(false);
 
   //Função para filtrar filmes pelo gênero
@@ -25,35 +24,6 @@ export default function Categoria({ categoria, index, id }) {
       setActive(!active); //Altera a estilização do botão
     }
   };
-
-  useEffect(
-    () => {
-      //Se o array estiver preenchido por algum gênero os filmes irão aparecer baseados no gênero selecionado
-      if (filmesFiltrados.length > 0) {
-        const carregarFilmes = async () => {
-          const response = await fetch(
-            `https://api.themoviedb.org/3/discover/movie?api_key=83a924a233a6fae4e8bb3ece72e1dcd0&with_genres=${filmesFiltrados}&language=pt-BR`
-          );
-          const body = await response.json();
-          setFilmes(body);
-        };
-        carregarFilmes();
-      }
-      //Se o array não receber nenhum gênero os filmes populares irão aparecer
-      else {
-        const load = async () => {
-          const response = await fetch(
-            `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR&page=${pagina}`
-          );
-          const body = await response.json();
-          setFilmes(body);
-        };
-        load();
-      }
-    },
-    //useEffect irá executar novamente se a variavel for alterada
-    [filmesFiltrados]
-  );
 
   return (
     <div
--- src/components/Filmes/Filmes.js
+++ src/components/Filmes/Filmes.js
@@ -4,22 +4,37 @@ import Filme from "../Filme/Filme";
 import "./Filmes.css";
 
 export default function Filmes() {
-  const { pagina, filmes, setFilmes, API_KEY } = useContext(Context);
+  const { pagina, filmes, setFilmes, API_KEY, filmesFiltrados } =
+    useContext(Context);
 
-  //useEffect para renderizar os filmes populares, com a opção de alterar a página
+  //useEffect para renderizar os filmes, com a opção de alterar a página quando o filtro não estiver sendo utilizado
   useEffect(
     () => {
-      const load = async () => {
-        const response = await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR&page=${pagina}`
-        );
-        const body = await response.json();
-        setFilmes(body);
-      };
-      load();
+      //Se o array estiver preenchido por algum gênero os filmes irão aparecer baseados no gênero selecionado
+      if (filmesFiltrados.length > 0) {
+        const carregarFilmes = async () => {
+          const response = await fetch(
+            `https://api.themoviedb.org/3/discover/movie?api_key=83a924a233a6fae4e8bb3ece72e1dcd0&with_genres=${filmesFiltrados}&language=pt-BR`
+          );
+          const body = await response.json();
+          setFilmes(body);
+        };
+        carregarFilmes();
+      }
+      //Se o array não receber nenhum gênero os filmes populares irão aparecer
+      else {
+        const load = async () => {
+          const response = await fetch(
+            `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR&page=${pagina}`
+          );
+          const body = await response.json();
+          setFilmes(body);
+        };
+        load();
+      }
     },
     //useEffect irá executar novamente se a variavel for alterada
-    [pagina]
+    [pagina, filmesFiltrados]
   );
 
   return (
